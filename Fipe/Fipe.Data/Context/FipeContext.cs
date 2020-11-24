f1a889639e144ec3dd92b6c1bacc9b38e0e08a73
suporte <suporte@STFBHZC044043-L>
2020-11-24 19:49:54 -0300

    Configuração inicial da classe de contexto e da string de conexão com o banco de dados.

---
+++ Fipe/Fipe.Data/Context/FipeContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using System.IO;
+
+namespace Fipe.Data.Context
+{
+    public class FipeContext : DbContext
+    {
+        public FipeContext(DbContextOptions options) : base(options) { }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            var config = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+            optionsBuilder.UseSqlServer(config.GetConnectionString("Fipe"));
+        }
+    }
+}
