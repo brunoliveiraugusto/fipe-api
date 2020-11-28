d0a7fe63b23b11ee836e8d854e1bb9d728e0ff9a
suporte <suporte@STFBHZC044043-L>
2020-11-28 10:55:45 -0300

    Criada classe abstrata para representar as requisições

---
+++ Fipe/Fipe.Integration/Request/BaseRequest.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fipe.Integration.Request
+{
+    public abstract class BaseRequest
+    {
+        public abstract Task<T> GetRequestAsync<T>();
+        public abstract IEnumerable<Task<T>> GetRequestListAsync<T>();
+    }
+}
