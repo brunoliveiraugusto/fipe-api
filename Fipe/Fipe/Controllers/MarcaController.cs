77b48962ea2b6151f1e1d3e25e84237818f5837a
suporte <suporte@STFBHZC044043-L>
2020-11-28 11:53:10 -0300

    Adicionada controller para marca e api para popular as marcas.

---
+++ Fipe/Fipe/Controllers/MarcaController.cs
@@ -0,0 +1,27 @@
+﻿using Fipe.Application.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Fipe.Api.Controllers
+{
+    [Route("api/marca")]
+    public class MarcaController : Controller
+    {
+        private readonly IMarcaAppService _marcaAppService;
+
+        public MarcaController(IMarcaAppService marcaAppService)
+        {
+            _marcaAppService = marcaAppService;
+        }
+
+        [HttpPost]
+        [Route("PopularMarcasFipe")]
+        public async Task PopularMarcasFipe()
+        {
+            await _marcaAppService.PopularMarcasObtidasApiFipe();
+        }
+    }
+}
