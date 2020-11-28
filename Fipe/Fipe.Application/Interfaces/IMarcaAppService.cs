18d0309aba0ae30d772f00950a11f0ee925f5d98
suporte <suporte@STFBHZC044043-L>
2020-11-28 11:55:11 -0300

    Criado service e interface da marca

---
+++ Fipe/Fipe.Application/Interfaces/IMarcaAppService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fipe.Application.Interfaces
+{
+    public interface IMarcaAppService
+    {
+        Task PopularMarcasObtidasApiFipe();
+    }
+}
