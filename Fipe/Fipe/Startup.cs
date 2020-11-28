d750823d43867001ed19f08e5db784a6e74ec2d1
suporte <suporte@STFBHZC044043-L>
2020-11-28 11:54:37 -0300

    Adicionada injeção do service da marca no contêiner de injeção de dependência.

--- Fipe/Fipe/Startup.cs
+++ Fipe/Fipe/Startup.cs
@@ -2,6 +2,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Fipe.Application.Interfaces;
+using Fipe.Application.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
@@ -16,6 +18,8 @@ namespace Fipe
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            //AppService
+            services.AddScoped<IMarcaAppService, MarcaAppService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
