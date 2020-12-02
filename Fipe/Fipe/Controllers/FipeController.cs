2775bd75cdca64e04a13f40b04df7966e216f23a
suporte <suporte@STFBHZC044043-L>
2020-12-02 18:00:59 -0300

    Criado novo controller e adicionado ao contêiner de injeção de dependência.

---
+++ Fipe/Fipe/Controllers/FipeController.cs
@@ -0,0 +1,27 @@
+﻿using Fipe.Application.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Fipe.Api.Controllers
+{
+    [Route("api/fipe")]
+    public class FipeController : Controller
+    {
+        private readonly IFipeAppService _fipe;
+
+        public FipeController(IFipeAppService fipe)
+        {
+            _fipe = fipe;
+        }
+
+        [HttpPost]
+        [Route("popularDadosFipe")]
+        public async Task PopularDadosFipe()
+        {
+            await _fipe.PopularDadosObtidosApiFipeAsync();
+        }
+    }
+}
