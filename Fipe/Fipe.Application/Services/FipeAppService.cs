bb13196778d9058b5ef5d60edf1eb4c9639d4090
suporte <suporte@STFBHZC044043-L>
2020-12-02 18:02:14 -0300

    Criada nova classe FipeAppService e sua interface

---
+++ Fipe/Fipe.Application/Services/FipeAppService.cs
@@ -0,0 +1,26 @@
+﻿using Fipe.Application.Interfaces;
+using Fipe.Data.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fipe.Application.Services
+{
+    public class FipeAppService : IFipeAppService
+    {
+        private readonly IParametroRepository _parametroRepository;
+        private readonly IMarcaAppService _marcaAppService;
+
+        public FipeAppService(IParametroRepository parametroRepository, IMarcaAppService marcaAppService)
+        {
+            _parametroRepository = parametroRepository;
+            _marcaAppService = marcaAppService;
+        }
+
+        public Task PopularDadosObtidosApiFipeAsync()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
