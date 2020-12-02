a6a5f2d356e100797b845756a8f80a7495fa5a15
suporte <suporte@STFBHZC044043-L>
2020-12-02 17:47:56 -0300

    Criado enum TipoVeiculo

---
+++ Fipe/Fipe.Application/Enums/TipoVeiculo.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Fipe.Application.Enums
+{
+    public enum TipoVeiculo : int
+    {
+        Carro = 1,
+        Moto = 2,
+        Caminhao = 3
+    }
+}
