58018289153a5c5551438dd7e35cf05b9ae07c48
suporte <suporte@STFBHZC044043-L>
2020-12-03 14:00:12 -0300

    Ajuste do método PopularMarcasObtidasApiFipeAsync para preencher o IdTipoVeiculo e gravar as marcas buscadas.

--- Fipe/Fipe.Application/Services/MarcaAppService.cs
+++ Fipe/Fipe.Application/Services/MarcaAppService.cs
@@ -1,10 +1,14 @@
 ﻿using Fipe.Application.Interfaces;
+using Fipe.Data.Entities;
 using Fipe.Data.Interfaces;
 using Fipe.Integration;
 using Fipe.Integration.ModelsRequest;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
+using AutoMapper;
+using Fipe.Application.AutoMapper;
 
 namespace Fipe.Application.Services
 {
@@ -12,24 +16,41 @@ namespace Fipe.Application.Services
     {
         private readonly IMarcaRequest _marcaRequest;
         private readonly IParametroRepository _parametroRepository;
+        private readonly IMarcaRepository _marcaRepository;
+        private readonly ITipoVeiculoRepository _tipoVeiculoRepository;
         
-        public MarcaAppService(IMarcaRequest marcaRequest, IParametroRepository parametroRepository)
+        public MarcaAppService(IMarcaRequest marcaRequest, IParametroRepository parametroRepository, IMarcaRepository marcaRepository,
+                ITipoVeiculoRepository tipoVeiculoRepository)
         {
             _marcaRequest = marcaRequest;
             _parametroRepository = parametroRepository;
+            _marcaRepository = marcaRepository;
+            _tipoVeiculoRepository = tipoVeiculoRepository;
         }
 
         public async Task PopularMarcasObtidasApiFipeAsync()
         {
-            string urlBaseApiFipe = await _parametroRepository.ObterValorParametroPorDescricaoAsync("BaseEndPointFipe");
-            var tiposVeiculos = await _parametroRepository.ObterValorParametroExpressionAsync(parametro => parametro.NomeParametro.Contains("EndPointFipe") && 
-                parametro.Valor.Contains("/marcas.json"));
-            
+            Task<string> urlBaseApiFipeTask = _parametroRepository.ObterValorParametroPorDescricaoAsync("BaseEndPointFipe");
+            Task<IEnumerable<Parametro>> parametrosTask = _parametroRepository.ObterParametrosAsync(parametro => parametro.NomeParametro.Contains("EndPointFipe") && parametro.Valor.Contains("/marcas.json"));
+            Task<IEnumerable<TipoVeiculo>> tiposVeiculoTask = _tipoVeiculoRepository.ObterTiposVeiculoAsync();
+
+            var urlBaseApiFipe = await urlBaseApiFipeTask;
+            var parametros = await parametrosTask;
+            var tiposVeiculo = await tiposVeiculoTask;
+
             var dataReferencia = DateTime.Now;
 
-            foreach(var tipoVeiculo in tiposVeiculos)
+            foreach(var parametro in parametros)
             {
-                IEnumerable<MarcaModelRequest> marcas = await _marcaRequest.ObterMarcasFipeApi(urlBaseApiFipe, tipoVeiculo);
+                IEnumerable<MarcaModelRequest> marcas = await _marcaRequest.ObterMarcasFipeApi(urlBaseApiFipe, parametro.Valor);
+                
+                marcas.ToList().ForEach((marca) =>
+                {
+                    marca.IdTipoVeiculo = tiposVeiculo.FirstOrDefault(tipoVeiculo => tipoVeiculo.Descricao.ToLower().Contains(parametro.NomeParametro.ToLower()) && 
+                                                                        tipoVeiculo.Descricao.ToLower().Contains(parametro.Valor.ToLower())).IdTipoVeiculo;
+                });
+
+                await _marcaRepository.GravarMarcasAsync(Mapper.Map<IEnumerable<Marca>>(marcas));
             }
         }
     }
