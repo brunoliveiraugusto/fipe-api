59bb03039b91423a4988fbb3032f6ae50cc109ca
suporte <suporte@STFBHZC044043-L>
2020-12-03 10:45:31 -0300

    Adicionada classe para realizar automapper de datarequest para uma entidade.

---
+++ Fipe/Fipe.Application/AutoMapper/DataRequestToEntitie.cs
@@ -0,0 +1,27 @@
+﻿using AutoMapper;
+using Fipe.Data.Entities;
+using Fipe.Integration.ModelsRequest;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Fipe.Application.AutoMapper
+{
+    public class DataRequestToEntitie : Profile
+    {
+        public DataRequestToEntitie()
+        {
+            CreateMap<MarcaModelRequest, Marca>()
+                .ForMember(dest => dest.Nome, opt => opt.MapFrom(x => x.Nome))
+                .ForMember(dest => dest.NomeFipe, opt => opt.MapFrom(x => x.NomeFipe))
+                .ForMember(dest => dest.OrderFipe, opt => opt.MapFrom(x => x.OrderFipe))
+                .ForMember(dest => dest.Chave, opt => opt.MapFrom(x => x.Chave))
+                .ForMember(dest => dest.IdMarcaFipe, opt => opt.MapFrom(x => x.IdMarcaFipe))
+                .ForMember(dest => dest.DataReferencia, opt => opt.MapFrom(x => DateTime.Now))
+                .ForMember(dest => dest.IdMarca, opt => opt.Ignore())
+                .ForMember(dest => dest.IdTipoVeiculo, opt => opt.Ignore())
+                .ForMember(dest => dest.TipoVeiculo, opt => opt.Ignore());
+
+        }
+    }
+}
