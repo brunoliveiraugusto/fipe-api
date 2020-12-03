b4cd22b58a1c57e03ddb7d5fbf703087e0b4b9ba
suporte <suporte@STFBHZC044043-L>
2020-12-03 10:48:20 -0300

    Criada entidade TipoVeiculo

---
+++ Fipe/Fipe.Data/Entities/TipoVeiculo.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Fipe.Data.Entities
+{
+    public class TipoVeiculo
+    {
+        public int IdTipoVeiculo { get; set; }
+        public string Descricao { get; set; }
+        public DateTime DataCadastro { get; set; }
+    }
+}
