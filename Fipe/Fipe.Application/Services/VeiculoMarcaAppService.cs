76858672600248e757096159879c54875d333b79
suporte <suporte@STFBHZC044043-L>
2020-12-05 13:05:01 -0300

    Criada classe de serviço e sua interface.

---
+++ Fipe/Fipe.Application/Services/VeiculoMarcaAppService.cs
@@ -0,0 +1,16 @@
+﻿using Fipe.Application.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fipe.Application.Services
+{
+    public class VeiculoMarcaAppService : IVeiculoMarcaAppService
+    {
+        public async Task PopularVeiculosMarcaObtidosApiFipeSync()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
