b58917bcbb824e43588a6814e3163a88dacea37e
suporte <suporte@STFBHZC044043-L>
2020-12-05 13:08:17 -0300

    Criada classe MarcaMap para mapear relacionamento com banco de dados.

---
+++ Fipe/Fipe.Data/Mappings/MarcaMap.cs
@@ -0,0 +1,17 @@
+﻿using Fipe.Data.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Fipe.Data.Mappings
+{
+    public class MarcaMap : IEntityTypeConfiguration<Marca>
+    {
+        public void Configure(EntityTypeBuilder<Marca> builder)
+        {
+            
+        }
+    }
+}
