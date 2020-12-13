62cd40e085f47d71a5c0904bcd5dd85fa2e84919
suporte <suporte@STFBHZC044043-L>
2020-12-13 11:27:58 -0300

    Criada classe de repositório para o log e método para gravar.

---
+++ Fipe/Fipe.Data/Interfaces/ILogFipeRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fipe.Data.Interfaces
+{
+    public interface ILogFipeRepository
+    {
+
+        Task GravarLog(string tipoExcecao, string mensagemExcecao, string rastreamento, DateTime dataExcecao);
+    }
+}
---
+++ Fipe/Fipe.Data/Repository/LogFipeRepository.cs
@@ -0,0 +1,23 @@
+﻿using Fipe.Data.Context;
+using Fipe.Data.Entities;
+using Fipe.Data.Interfaces;
+using System;
+using System.Threading.Tasks;
+
+namespace Fipe.Data.Repository
+{
+    public class LogFipeRepository : ILogFipeRepository
+    {
+        private readonly FipeContext _context;
+
+        public LogFipeRepository(FipeContext context)
+        {
+            _context = context;
+        }
+
+        public async Task GravarLog(string tipoExcecao, string mensagemExcecao, string rastreamento, DateTime dataExcecao)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
