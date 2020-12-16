b947628ce1b36a2725bd9a60039ccab46ff53efa
suporte <suporte@STFBHZC044043-L>
2020-12-16 18:07:48 -0300

    Criada exceção para requisições inválidas.

---
+++ Fipe/Fipe.Generics/Exceptions/InvalidRequestException.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Fipe.Generics.Exceptions
+{
+    public class InvalidRequestException : Exception
+    {
+        public InvalidRequestException(string message) : base(message)
+        {
+
+        }
+    }
+}
