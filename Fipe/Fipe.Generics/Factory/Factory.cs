038c70fa0451bb4d18ceae660be26ca4b4c7d416
suporte <suporte@STFBHZC044043-L>
2020-12-16 18:06:16 -0300

    Criado método Factory para retornar um tipo concreto.

---
+++ Fipe/Fipe.Generics/Factory/Factory.cs
@@ -0,0 +1,12 @@
+﻿using Fipe.Generics.Factory.Interface;
+
+namespace Fipe.Generics.Factory
+{
+    public class Factory : IFactory
+    {
+        public T GetFactory<T>() where T : class, new()
+        {
+            return new T();
+        }
+    }
+}
---
+++ Fipe/Fipe.Generics/Factory/Interface/IFactory.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Fipe.Generics.Factory.Interface
+{
+    public interface IFactory
+    {
+        T GetFactory<T>() where T : class, new();
+    }
+}
