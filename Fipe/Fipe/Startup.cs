e202ec8485e18e783e3e37814ce1c09714f06603
suporte <suporte@STFBHZC044043-L>
2020-12-17 19:30:49 -0300

    Configurações iniciais de integração da api com o swagger.

--- Fipe/Fipe/Startup.cs
+++ Fipe/Fipe/Startup.cs
@@ -11,11 +11,11 @@ using Fipe.Integration;
 using Fipe.Integration.Request;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
 using System.Net.Http;
 
 namespace Fipe
@@ -65,6 +65,13 @@ namespace Fipe
             IMapper mapper = mappingConfig.CreateMapper();
             services.AddSingleton(mapper);
             #endregion
+
+            #region Swagger
+            services.AddSwaggerGen(s =>
+            {
+                s.SwaggerDoc("v1", new OpenApiInfo { Title = "Fipe API", Version = "v1" });
+            });
+            #endregion
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -80,6 +87,13 @@ namespace Fipe
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
+            });
+
+            app.UseSwagger();
+
+            app.UseSwaggerUI(opt =>
+            {
+                opt.SwaggerEndpoint("/swagger/v1/swagger.json", "Fipe API");
             });
         }
     }
