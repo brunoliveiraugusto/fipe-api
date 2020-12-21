ac8a4df5678a5c7bbd29b686ecb4a5b5472a411d
suporte <suporte@STFBHZC044043-L>
2020-12-21 10:44:08 -0300

    Criado método para buscar veículos de determinada marca.

---
+++ Fipe/Fipe.Integration/Interfaces/IVeiculoMarcaRequest.cs
@@ -0,0 +1,11 @@
+﻿using Fipe.Integration.ModelsRequest;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Fipe.Integration.Interfaces
+{
+    public interface IVeiculoMarcaRequest
+    {
+        Task<IEnumerable<VeiculoMarcaModelRequest>> ObterVeiculosMarcaFipeApiAsync(string baseUrl, string url);
+    }
+}
---
+++ Fipe/Fipe.Integration/Request/VeiculoMarcaRequest.cs
@@ -0,0 +1,22 @@
+﻿using Fipe.Integration.Interfaces;
+using Fipe.Integration.ModelsRequest;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace Fipe.Integration.Request
+{
+    public class VeiculoMarcaRequest : BaseRequest, IVeiculoMarcaRequest
+    {
+        public VeiculoMarcaRequest(HttpClient client) : base(client)
+        {
+
+        }
+
+        public async Task<IEnumerable<VeiculoMarcaModelRequest>> ObterVeiculosMarcaFipeApiAsync(string baseUrl, string url)
+        {
+            return await GetRequestListAsync<VeiculoMarcaModelRequest>(baseUrl, url);
+        }
+    }
+}
