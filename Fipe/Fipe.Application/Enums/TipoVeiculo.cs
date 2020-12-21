c30b5896e495908eb0c6011eb461dbbb268c0522
suporte <suporte@STFBHZC044043-L>
2020-12-21 11:53:44 -0300

    Ajuste de tipo do enum.

--- Fipe/Fipe.Application/Enums/TipoVeiculo.cs
+++ Fipe/Fipe.Application/Enums/TipoVeiculo.cs
@@ -1,13 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace Fipe.Application.Enums
+﻿namespace Fipe.Application.Enums
 {
-    public enum TipoVeiculo : int
+    public enum TipoVeiculo
     {
-        Carro = 1,
-        Moto = 2,
-        Caminhao = 3
+        Carros,
+        Motos,
+        Caminhoes
     }
 }
