8a7d8717a256da58a7a61b74ffb388542c260908
suporte <suporte@STFBHZC044043-L>
2020-12-21 11:55:22 -0300

    Criado método para gravar os veículos de cada marca.

---
+++ Fipe/Fipe.Data/Repository/VeiculoMarcaRepository.cs
@@ -0,0 +1,25 @@
+﻿using Fipe.Data.Context;
+using Fipe.Data.Entities;
+using Fipe.Data.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fipe.Data.Repository
+{
+    public class VeiculoMarcaRepository : IVeiculoMarcaRepository
+    {
+        private readonly FipeContext _context;
+
+        public VeiculoMarcaRepository(FipeContext context)
+        {
+            _context = context;
+        }
+
+        public async Task GravarVeiculosMarcaAsync(IEnumerable<VeiculoMarca> veiculosMarca)
+        {
+            await _context.SaveChangesListAsync(veiculosMarca);
+        }
+    }
+}
